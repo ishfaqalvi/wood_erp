6521fadb0cea18fcac5fd7b072a108aad11faa6a
Ishfaq Alvi <ishfaq.alvi.33@gmail.com>
2023-11-24 18:11:43 +0500

    Login route added

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
+Route::get('/', function(){
+    return redirect()->route('login');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Protected Routes
